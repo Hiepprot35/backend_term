a2dcd0bbab6afe334d29f8a12772f3e53f1fb4b7
Tuan Hiep <tansou57@gmail.com>
2023-10-25 14:01:47 +0700

    oke

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -4,11 +4,12 @@ const dbconnection = mysql.createConnection(
     {
         host: process.env.DB_host,
         user: process.env.DB_user,
-        password:"",
+        password:"hiepprot35",
         port:"3306",
         database: process.env.DB_database
     }
 )
+console.log(process.env.DB_password)
 async function connect() {
     try {
 
