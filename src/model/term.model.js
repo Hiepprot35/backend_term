9dd8afc25d27424e7152a56e8fd93662e59b1ed2
Hiepprot3 <tansou57@gmail.com>
2023-11-02 00:48:03 +0700

    oke

--- src/model/term.model.js
+++ src/model/term.model.js
@@ -2,7 +2,7 @@ const { dbconnection } = require('../config/dbconfig');
 class TermModel {
     async getAllTermModel(data) {
         const result = await new Promise(function (resolve, reject) {
-            dbconnection.query('SELECT * FROM term where title=?',[data.title], (err, result) => {
+            dbconnection.query('SELECT * FROM term where title=?', [data.title], (err, result) => {
                 if (result) {
                     resolve(result);
                 } else {
@@ -18,10 +18,10 @@ class TermModel {
         // Tạo một mảng các promise cho từng truy vấn INSERT
         try {
             const insertPromises = data.map(term => {
-                const img=term.HinhAnh?.data
-                const  HexDataImg=img && img.map(byte => byte.toString(16).padStart(2, '0')).join('');
-                const binaryImg=HexDataImg&& Buffer.from(HexDataImg, 'hex');
-    
+                const img = term.HinhAnh?.data
+                const HexDataImg = img && img.map(byte => byte.toString(16).padStart(2, '0')).join('');
+                const binaryImg = HexDataImg && Buffer.from(HexDataImg, 'hex');
+
                 const dataFinal = { ...term, HinhAnh: binaryImg || "IMG" }
                 console.log(dataFinal)
                 return new Promise((resolve, reject) => {
@@ -39,11 +39,12 @@ class TermModel {
             console.log(error)
         }
     }
-    async getAllTitle()
-    {
+    async getAllTitle() {
         try {
             const result = await new Promise(function (resolve, reject) {
-                dbconnection.query('SELECT title,Mota FROM `term`GROUP BY title', (err, result) => {
+                dbconnection.query(`SELECT term.title, term.Mota
+                FROM term
+                GROUP BY term.title, term.Mota`, (err, result) => {
                     if (result) {
                         resolve(result);
                     } else {
